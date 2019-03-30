41ad5f5c06c349b1fef8d5f056aad079f7a2c8ee
lutencjusz <lutencjusz@gmail.com>
2019-03-30 15:16:01 +0100

    commit

--- DemoJSON.py
+++ DemoJSON.py
@@ -2,6 +2,6 @@ import json
 data = {"jsonKey": "jsonValue","title": "hello world"}
 print (json.dumps(data, indent=4, separators=(". ", " = ")))
 
-with open('C:\Data\python\Demo_i_kurs\python\parametryCz.json') as jsonData:
+with open('\C:\Data\python\Demo_i_kurs\python\parametryCz.json') as jsonData:
     d = json.load(jsonData)
     print("VcMin: "+str(d["VcMin"]))
